68b201290ba933520bce53ff54acd29c9c19a67c
marsre <mariano.a.sreider@gmail.com>
2022-08-13 17:40:22 -0300

    MonetCafeJS

---
+++ data/productos.data.js
@@ -0,0 +1,155 @@
+const productos = [
+    {
+        categoria:1,
+        precio:700,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Colombian",
+        slug: "galeria1",
+
+        cantidad: 1,
+    },
+    {
+        categoria:1,
+        precio:750,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Peru",
+        slug: "galeria2",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:1,
+        precio:890,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Nicaragua",
+        slug: "galeria3",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:1,
+        precio:920,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Japan",
+        slug: "galeria4",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:1,
+        precio:950,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"China",
+        slug: "galeria4",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:1,
+        precio:910,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Brasil",
+        slug: "galeria4",
+        
+        cantidad: 1,
+    },
+    ///////////////////////////////////
+
+
+    {
+        categoria:2,
+        precio:400,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Torta Marquise",
+        slug: "galeria1",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:2,
+        precio:320,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Torta Marmolada",
+        slug: "galeria1",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:2,
+        precio:480,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Torta Velvet",
+        slug: "galeria1",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:2,
+        precio:390,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Torta Mousse",
+        slug: "galeria1",
+        
+        cantidad: 1,
+    },
+
+    ////////////////////////////
+    
+    {
+        categoria:3,
+        precio:180,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Filtro",
+        slug: "galeria2",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:3,
+        precio:1900,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Cafetera",
+        slug: "galeria2",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:3,
+        precio:1020,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Molinillo",
+        slug: "galeria2",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:3,
+        precio:580,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Volturno",
+        slug: "galeria2",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:3,
+        precio:780,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Vialetti",
+        slug: "galeria2",
+        
+        cantidad: 1,
+    },
+    {
+        categoria:3,
+        precio:280,
+        descripcion:  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia ea a debitis cum nulla eligendi! Deserunt vitae",
+        nombre:"Taza",
+        slug: "galeria2",
+        
+        cantidad: 1,
+    },
+    
+
+
+]
